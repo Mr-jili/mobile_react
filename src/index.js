e3fcbf59e78592925a53d5fe08f0b1d41b8f9f71
coderWangJun <1632869948@qq.com>
2018-01-29 14:16:36 +0800

    修复控制台index.js的路由跳转报bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter,Redirect,Route,Switch} from 'react-router-dom';
+import {HashRouter, Redirect, Route, Switch} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import App from './containers/App';
 import Cart from "./containers/Cart/Cart";
@@ -12,17 +12,17 @@ import store from './store';
 
 ReactDOM.render((
   <Provider store={store}>
-    <App>
-      <HashRouter>
+    <HashRouter>
+      <App>
         <Switch>
-          <Route path='/main' component={Main}>首页</Route>
-          <Route path='/goodscategory' component={Goodscategory}>分类</Route>
-          <Route path='/savour' component={Savour}>品味</Route>
-          <Route path='/cart' component={Cart}>购物车</Route>
-          <Route path='/usercenter' component={UserCenter}>个人</Route>
+          <Route path='/' component={Main}/>
+          <Route path='/goodscategory' component={Goodscategory}/>
+          <Route path='/savour' component={Savour}/>
+          <Route path='/cart' component={Cart}/>
+          <Route path='/usercenter' component={UserCenter}/>
           <Redirect to='/'/>
         </Switch>
-      </HashRouter>
-    </App>
+      </App>
+    </HashRouter>
   </Provider>
 ), document.getElementById('root'));
