b7b7a343a2a6c8a83aa1983a921405c8b7f27829
jili <jili52012@126.com>
2018-01-31 15:02:57 +0800

    品味加载更多完成

--- src/store/action-types.js
+++ src/store/action-types.js
@@ -1,8 +1,7 @@
 export const INIT_STATE = 'INIT_STATE';
 
-export const SET_SAVOUR='SET_SAVOUR'; //品味
 
-export const CHANGE_SAVOUR='CHANGE_SAVOUR'; //改变状态
+export const CHANGE_SAVOUR='CHANGE_SAVOUR'; // 品味改变状态
 
 //登录或注册时把获得的数据存在redux中
 export const SET_USER_INFO = "SET_USER_INFO";
