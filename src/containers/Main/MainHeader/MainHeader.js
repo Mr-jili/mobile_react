b920276749fa8c41e2b9e1f6c364e3c0489c7840
MillionChen <450413474@qq.com>
2018-02-01 21:58:12 +0800

    首页跳转完成 只有手机可以跳转，收藏页面完成

--- src/containers/Main/MainHeader/MainHeader.js
+++ src/containers/Main/MainHeader/MainHeader.js
@@ -1,14 +1,20 @@
 import React ,{ Component} from "react";
-import logo from  "../../../images/navi_title_v2.png"
+import logo from  "../../../images/navi_title_v2.png";
+import  {withRouter} from "react-router-dom"
 import "./index.less";
-export default class MainHeader extends Component{
+
+ class MainHeader extends Component{
+
+
     render(){
         return(
             <div className="cwj-main-header">
                 <div className="logo-box">
                     <img src={logo} alt="123"/>
                 </div>
-                <div className="search-box">
+                <div className="search-box" onClick={()=>{
+                    this.props.history.push("/search")
+                }}>
                     <img src="https://static.home.mi.com/youpin/static/m/res/images/home_search.png" alt="123"/>
                     <span>年货嗨抢，最高直降1000</span>
                 </div>
@@ -20,3 +26,4 @@ export default class MainHeader extends Component{
         )
     }
 }
+export default  withRouter(MainHeader)
