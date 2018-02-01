04d370f325beda9e6261f22bb219aea569e7f588
jili <jili52012@126.com>
2018-02-01 13:39:47 +0800

    组件拆分

---
+++ src/containers/Detaill/DetailEvaluate.js
@@ -0,0 +1,58 @@
+import React,{Component} from 'react'
+export default class DetailEvaluate extends Component{
+    render(){
+        return (
+            <div>
+                <div className="detail_user clearfix">
+                    <ul>
+                        <li>
+                            <div className="user_outer">
+                                <img src="https://s1.mi-img.com/mfsv2/avatar/s008/p01FmRF48sPx/Zp3v1owrnpnqdl.jpg" alt=""/>
+                                <span className="span1">啦啦啦啦啦</span>
+                                <span className="span2">2018.01.31 00:44</span>
+                            </div>
+                            <div className="p">
+                                还好，不过没达到预期。
+                            </div>
+                            <div className="detail_hengxian"> </div>
+                        </li>
+                        <li>
+                            <div className="user_outer">
+                                <img src="https://s1.mi-img.com/mfsv2/avatar/s008/p01FmRF48sPx/Zp3v1owrnpnqdl.jpg" alt=""/>
+                                <span className="span1">啦啦啦啦啦</span>
+                                <span className="span2">2018.01.31 00:44</span>
+                            </div>
+                            <div className="p">
+                                还好，不过没达到预期。
+                            </div>
+                            <div className="detail_hengxian"> </div>
+                        </li>
+                        <li>
+                            <div className="user_outer">
+                                <img src="https://s1.mi-img.com/mfsv2/avatar/s008/p01FmRF48sPx/Zp3v1owrnpnqdl.jpg" alt=""/>
+                                <span className="span1">啦啦啦啦啦</span>
+                                <span className="span2">2018.01.31 00:44</span>
+                            </div>
+                            <div className="p">
+                                还好，不过没达到预期。
+                            </div>
+                            <div className="detail_hengxian"> </div>
+                        </li>
+
+                        <li>
+                            <div className="user_outer">
+                                <img src="https://s1.mi-img.com/mfsv2/avatar/s008/p01FmRF48sPx/Zp3v1owrnpnqdl.jpg" alt=""/>
+                                <span className="span1">啦啦啦啦啦</span>
+                                <span className="span2">2018.01.31 00:44</span>
+                            </div>
+                            <div className="p">
+                                还好，不过没达到预期。
+                            </div>
+                            <div className="detail_hengxian"> </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
