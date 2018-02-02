9b84ecac732bdbce8f4b65794ce1782c84c73ed3
CherryLeee <915105421@qq.com>
2018-02-02 12:56:01 +0800

    订单页面搭建个人中心页面跳转

--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@ import Collection from "./containers/Collection/Collection";
 import Collections from "./containers/Collection/Collections";
 
 import OrderList from "./containers/OrderList/OrderList";
+import Collections from "./containers/Collection/Collections";
+
 ReactDOM.render((
   <Provider store={store}>
     <HashRouter>
