9178b7d76e93ab61d295e872a3e9dcada080d7f9
YangBo <1043951234@qq.com>
2018-02-08 18:58:35 +0800

    ''

--- src/containers/Cart/common/DialogCart.js
+++ src/containers/Cart/common/DialogCart.js
@@ -4,8 +4,6 @@ import "./DialogCart.less";
 export default class DialogCart extends Component {
   render() {
     let {ifShow, text} = this.props;
-    console.log(this.props);
-
     return (
       <div style={ifShow ? {display: "block"} : {display: "none"}} className="dialog-cart">
         <p>{text}</p>
